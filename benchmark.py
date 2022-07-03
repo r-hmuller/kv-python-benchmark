bc8443bf9fbd311a50e3eecc747257ddce703291
Rodrigo Müller <rodrigo_hmuller@hotmail.com>
2022-07-03 18:18:30 -0300

    Initial commit

---
+++ benchmark.py
@@ -0,0 +1,31 @@
+import datetime
+import random
+from random import choice
+from string import ascii_lowercase
+import time
+
+import requests
+
+
+# Time in seconds
+# Path of the file to save the results
+# The URL to send POST commands
+def execute(time, url, file_to_save):
+    endTime = datetime.datetime.now() + datetime.timedelta(seconds=time)
+    latencies = {}
+    while datetime.datetime.now() < endTime:
+        if random.randint(0, 50) == 22:
+            start_time = time.time()
+            r = requests.post(url=url, data={'key': "".join(choice(ascii_lowercase) for i in range(18)),
+                                             'value': "".join(choice(ascii_lowercase) for i in range(50))})
+            end_time = time.time()
+            latency = end_time - start_time
+            latencies[start_time] = latency
+        else:
+            r = requests.post(url=url, data={'key': "".join(choice(ascii_lowercase) for i in range(18)),
+                                             'value': "".join(choice(ascii_lowercase) for i in range(50))})
+        time.sleep(0.2)
+
+    with open(file_to_save, 'a') as f:
+        for key, value in latencies.items():
+            f.write('%s,%s\n' % (key, value))
---
+++ main.py
@@ -0,0 +1,28 @@
+# Press the green button in the gutter to run the script.
+import multiprocessing
+import sys
+
+import benchmark
+
+if __name__ == '__main__':
+    #1 Num thread
+    #2 Seconds to run
+    #3 URL
+    #4 File to write
+    arguments = sys.argv[1:]
+    jobs = []
+
+    for i in range(int(arguments[0])):
+        process = multiprocessing.Process(
+            target=benchmark.execute,
+            args=(int(arguments[1]), arguments[2], arguments[3])
+        )
+        jobs.append(process)
+
+    for j in jobs:
+        j.start()
+
+    for j in jobs:
+        j.join()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
