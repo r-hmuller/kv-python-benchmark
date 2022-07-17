27c80265f364bff4b32cc54a6646aabfbb308f12
Rodrigo Müller <rodrigo_hmuller@hotmail.com>
2022-07-17 18:24:10 -0300

    Only save on file on first thread

--- benchmark.py
+++ benchmark.py
@@ -5,34 +5,37 @@ import time
 
 import requests
 
-
 # Time in seconds
 # Path of the file to save the results
 # The URL to send POST commands
 def execute(time, url, file_to_save, main_thread):
+    selected_key = randint(0, 1_000_000)
+    new_value = "".join(choice(ascii_lowercase) for i in range(1024))
     if main_thread is True:
-        requests.post(url=f"{url}/testing", data={'action': 'start'})
         requests.post(url=f"{url}/seed", data={'quantity': 1_000_000, 'size': 1024})
+        requests.post(url=f"{url}/testing", data={'action': 'start'})
+        endTime = datetime.datetime.now() + datetime.timedelta(seconds=time)
+        latencies = {}
+        while datetime.datetime.now() < endTime:
+            if randint(0, 50) == 22:
+                start_time = time.time()
+                requests.post(url=url, data={'key': selected_key,
+                                             'value': new_value})
+                end_time = time.time()
+                latency = end_time - start_time
+                latencies[start_time] = latency
+            else:
+                requests.post(url=url, data={'key': selected_key,
+                                             'value': new_value})
+            time.sleep(0.2)
 
-    endTime = datetime.datetime.now() + datetime.timedelta(seconds=time)
-    latencies = {}
-    while datetime.datetime.now() < endTime:
-        selected_key = randint(0, 1_000_000)
-        new_value = "".join(choice(ascii_lowercase) for i in range(1024))
-        if randint(0, 50) == 22:
-            start_time = time.time()
-            requests.post(url=url, data={'key': selected_key,
-                                             'value': new_value})
-            end_time = time.time()
-            latency = end_time - start_time
-            latencies[start_time] = latency
-        else:
-            requests.post(url=url, data={'key': selected_key,
-                                             'value': new_value})
+        requests.post(url=f"{url}/testing", data={'action': 'stop'})
+
+        with open(file_to_save, 'a') as f:
+            for key, value in latencies.items():
+                f.write('%s,%s\n' % (key, value))
+    else:
+        requests.post(url=url, data={'key': selected_key,
+                                     'value': new_value})
         time.sleep(0.2)
 
-    if main_thread is True:
-        requests.post(url=f"{url}/testing", data={'action': 'stop'})
-    with open(file_to_save, 'a') as f:
-        for key, value in latencies.items():
-            f.write('%s,%s\n' % (key, value))
