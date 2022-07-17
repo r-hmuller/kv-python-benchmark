413982d112888f578b315a58aceca6b861752af1
Rodrigo Müller <rodrigo_hmuller@hotmail.com>
2022-07-17 19:36:39 -0300

    Include more logging

--- benchmark.py
+++ benchmark.py
@@ -40,6 +40,8 @@ def execute(time_to_run, url, file_to_save, main_thread, main_client, should_see
         print("Finishing test")
         r = requests.post(url=f"{url}/testing", data={'action': 'stop', 'path': f"/users/Hmuller/logs/{arguments[1]}_clients_{arguments[0]}_threads.log"})
         print(f"Status: {r.status_code}")
+        if r.status_code != 204:
+            print(r.content)
 
         with open(file_to_save, 'a') as f:
             for key, value in latencies.items():
