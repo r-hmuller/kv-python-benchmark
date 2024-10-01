a19dce83ed455b9d6a69f9b58b684db98e1a4da7
Rodrigo Muller <r.muller@global.leaseweb.com>
2024-10-01 22:26:05 +0200

    Create simple file to compare keys

---
+++ compare-requests.py
@@ -0,0 +1,20 @@
+import sys
+
+import requests
+
+file_name = sys.argv[1]
+url = sys.argv[2]
+
+session = requests.Session()
+
+with open(file_name, 'r') as file:
+    for line in file:
+        values = line.split(';')
+        key = values[0]
+        value = values[1]
+
+        response = session.get(url=f"{url}?key={key}")
+        if response.text != value:
+            print(f"Key: {key} Value: {value} Response: {response.text}")
+
+print("Done")
